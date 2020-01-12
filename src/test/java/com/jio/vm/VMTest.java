e867a459dca9d89783f0942867cf7e889b419e3f
kishore12rao <kishore.koool@gmail.com>
2020-01-12 16:37:22 +0530

    removed unused imports

--- src/test/java/com/jio/vm/VMTest.java
+++ src/test/java/com/jio/vm/VMTest.java
@@ -1,8 +1,6 @@
 package com.jio.vm;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
