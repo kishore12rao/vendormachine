c02b8e795a503ddc777e762c62362864242b7a08
kishore12rao <kishore.koool@gmail.com>
2020-01-13 10:30:27 +0530

    application.properties

--- src/main/java/com/jio/vm/db/DBConnection.java
+++ src/main/java/com/jio/vm/db/DBConnection.java
@@ -1,23 +1,32 @@
 package com.jio.vm.db;
 
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-	
+
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		
-		String name = "root";
-		String password = "root";
-		String host = "localhost";
-		String db = "vending_machine";
 
-		Class.forName("com.mysql.jdbc.Driver");
-		String url = "jdbc:mysql://" + host + ":3306/" + db;
+		final Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream("E:\\eclipse-workspace\\JioVendingMachine\\src\\main\\resources\\application.properties"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		Connection connection = DriverManager.getConnection(url, name, password);
+		String driver =  properties.getProperty("spring.datasource.driver-class-name");
+
+		String username = properties.getProperty("spring.datasource.username");
+		String password = properties.getProperty("spring.datasource.password");
+
+		Class.forName(driver);
+		String url = properties.getProperty("spring.datasource.url");
+
+		Connection connection = DriverManager.getConnection(url, username, password);
 		connection.setAutoCommit(true);
 		return connection;
 	}
